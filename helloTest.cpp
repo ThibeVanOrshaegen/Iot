6bfc080bcb659da9621d00a6fea9704642aebf39
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-10-19 19:34:03 +0200

    Update helloTest.cpp

--- helloTest.cpp
+++ helloTest.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
-#include <cppunit/TestRunner.h>
-#include <cppunit/TestResult.h>
-#include <cppunit/TestResultCollector.h>
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/BriefTestProgressListener.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
 
 class Test : public CPPUNIT_NS::TestCase
 {
